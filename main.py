165900db63387855d465552d0c321ec4fe69c71d
armando-palacio <armandopr3009@gmail.com>
2023-11-17 18:52:11 -0300

    update

--- main.py
+++ main.py
@@ -37,166 +37,157 @@ for user in os.listdir(PATH_HISTORY):
         print('Se recuperó el chat de:', user)
 
 
+bot = telebot.TeleBot( os.environ['TELEGRAM_TOKEN'] )
 print('The bot is ready!!',end='\n\n\n')
 
-while True:
-    try:
-        bot = telebot.TeleBot( os.environ['TELEGRAM_TOKEN'] )
+
+@bot.message_handler(commands=['start'])
+def start(msg):
+    user = msg.from_user.username
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
+
+    chats[user].send_hello(bot) # envía el mensaje de bienvenida al usuario
+    chats[user].create_temp_file() # guarda el estado actual del chat en un archivo temporal ~temp.json
 
 
-        @bot.message_handler(commands=['start'])
-        def start(msg):
-            user = msg.from_user.username
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
-
-            chats[user].send_hello(bot) # envía el mensaje de bienvenida al usuario
-            chats[user].create_temp_file() # guarda el estado actual del chat en un archivo temporal ~temp.json
+@bot.message_handler(commands=['user'])
+def start(msg):
+    user = msg.from_user.username
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
+    bot.send_message(chat_id=msg.chat.id, text=f'El usuario actual es: @{user}')
 
 
-        @bot.message_handler(commands=['user'])
-        def start(msg):
-            user = msg.from_user.username
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
-            bot.send_message(chat_id=msg.chat.id, text=f'El usuario actual es: @{user}')
+@bot.message_handler(commands=['rol'])
+def start(msg):
+    user = msg.from_user.username
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
+    bot.send_message(chat_id=msg.chat.id, text=f'El rol actual es: {chats[user].role}')
 
 
-        @bot.message_handler(commands=['rol'])
-        def start(msg):
-            user = msg.from_user.username
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
-            bot.send_message(chat_id=msg.chat.id, text=f'El rol actual es: {chats[user].role}')
+@bot.message_handler(commands=['read'])
+def read_message(msg):
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
+    
+    if msg.content_type != 'text':
+        bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de texto que quieras que lea o escribe el texto luego del comando `/read` (ej: /read Hello Arche!)')
+        return
+    if msg.reply_to_message:
+        text = msg.reply_to_message.text
+    else:
+        text = msg.text.split('/read')[-1]
+
+    try: 
+        file = text2voice(text)
+        bot.send_voice(chat_id=msg.chat.id, voice=file)
+        file.close(); os.remove('temp.mp3')
+    except Exception as e:
+        print(e)
+        bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de texto que quieras que lea o escribe el texto luego del comando `/read` (ej: /read Hello Arche!)')
 
 
-        @bot.message_handler(commands=['read'])
-        def read_message(msg):
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
-            
-            if msg.content_type != 'text':
-                bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de texto que quieras que lea o escribe el texto luego del comando `/read` (ej: /read Hello Arche!)')
-                return
-            if msg.reply_to_message:
-                text = msg.reply_to_message.text
-            else:
-                text = msg.text.split('/read')[-1]
+@bot.message_handler(commands=['write'])
+def write_message(msg):
+    user = msg.from_user.username
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
 
-            try: 
-                file = text2voice(text)
-                bot.send_voice(chat_id=msg.chat.id, voice=file)
-                file.close(); os.remove('temp.mp3')
-            except Exception as e:
-                print(e)
-                bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de texto que quieras que lea o escribe el texto luego del comando `/read` (ej: /read Hello Arche!)')
+    if msg.reply_to_message:
+        if msg.reply_to_message.content_type != 'voice':
+            bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de voz que quieras que transcriba.')
+            return
+        chats[user].transcript_to_text(msg.reply_to_message, bot)
+    else:
+        bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de `voz` que quieras que transcriba.')
+        return
 
 
-        @bot.message_handler(commands=['write'])
-        def write_message(msg):
-            user = msg.from_user.username
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario y un archivo temporal ~temp.json en la carpeta del historial del usuario
-
-            if msg.reply_to_message:
-                if msg.reply_to_message.content_type != 'voice':
-                    bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de voz que quieras que transcriba.')
-                    return
-                chats[user].transcript_to_text(msg.reply_to_message, bot)
-            else:
-                bot.send_message(chat_id=msg.chat.id, text='Haz referencia a algún mensaje de `voz` que quieras que transcriba.')
-                return
-        
-
-        @bot.message_handler(commands=['newchat'])
-        def newchat(msg):
-            user = msg.from_user.username
-            if create_chat(msg, chats):
-                chats[user].send_hello(bot)
-            else:
-                chats[user].new_chat(bot)
+@bot.message_handler(commands=['newchat'])
+def newchat(msg):
+    user = msg.from_user.username
+    if create_chat(msg, chats):
+        chats[user].send_hello(bot)
+    else:
+        chats[user].new_chat(bot)
 
 
-        @bot.message_handler(commands=['load'])
-        def load_file_content(msg):
-            user = msg.from_user.username
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario
-            
-            _, contents = chats[user].get_contents_files(extension='.json')
+@bot.message_handler(commands=['load'])
+def load_file_content(msg):
+    user = msg.from_user.username
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario
+    
+    _, contents = chats[user].get_contents_files(extension='.json')
 
-            if not contents:
-                bot.send_message(chat_id=msg.chat.id, text='No se encontraron conversaciones previas 🤷‍♂️')
-            else:
-                menu = [[types.InlineKeyboardButton(text = content, callback_data=content.lower())] for content in contents]
-                keyboard = types.InlineKeyboardMarkup(menu)
-                bot.send_message(chat_id=msg.chat.id, text='Elije un contexto para cargar:', reply_markup=keyboard)
-
-
-        # maneja el comando \log
-        @bot.message_handler(commands=['log'])
-        def show_log(msg):
-            user = msg.from_user.username
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario
-            
-            actual_content = '\n' + '\n'.join(['* ' + item['content'] for item in chats[user].history[2:] if item['role']=='user'])
-            bot.send_message(chat_id=msg.chat.id, text=f'El contexto actual es: {actual_content}')
+    if not contents:
+        bot.send_message(chat_id=msg.chat.id, text='No se encontraron conversaciones previas 🤷‍♂️')
+    else:
+        menu = [[types.InlineKeyboardButton(text = content, callback_data=content.lower())] for content in contents]
+        keyboard = types.InlineKeyboardMarkup(menu)
+        bot.send_message(chat_id=msg.chat.id, text='Elije un contexto para cargar:', reply_markup=keyboard)
 
 
-        # maneja el comando \setrol
-        @bot.message_handler(commands=['setrol'])
-        def change_rol(msg):
-            create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario
-            
-            rol_menu = [[types.InlineKeyboardButton(text = item, callback_data=item.lower())] for item in ROLES.keys()]
-            keyboard = types.InlineKeyboardMarkup(rol_menu)
-            bot.send_message(chat_id=msg.chat.id, text='Elije un rol para ChatGPT:', reply_markup=keyboard)
+# maneja el comando \log
+@bot.message_handler(commands=['log'])
+def show_log(msg):
+    user = msg.from_user.username
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario
+    
+    actual_content = '\n' + '\n'.join(['* ' + item['content'] for item in chats[user].history[2:] if item['role']=='user'])
+    bot.send_message(chat_id=msg.chat.id, text=f'El contexto actual es: {actual_content}')
 
 
-        # maneja las entradas de texto al chat del bot
-        @bot.message_handler(content_types=['text'])
-        def reply_msg_handler(msg):    
-            user = msg.from_user.username
-            create_chat(msg, chats)
-
-            if msg.text.startswith('/'):
-                bot.send_message(chat_id=msg.chat.id, text='No puedo reconocer este comando 😕')
-                return
-            chats[user].response_to_text(msg, bot)
+# maneja el comando \setrol
+@bot.message_handler(commands=['setrol'])
+def change_rol(msg):
+    create_chat(msg, chats) # crea un nuevo chat SI no existe el usuario
+    
+    rol_menu = [[types.InlineKeyboardButton(text = item, callback_data=item.lower())] for item in ROLES.keys()]
+    keyboard = types.InlineKeyboardMarkup(rol_menu)
+    bot.send_message(chat_id=msg.chat.id, text='Elije un rol para ChatGPT:', reply_markup=keyboard)
 
 
-        # maneja las descargas de archivos de audio y voz 
-        @bot.message_handler(content_types=['voice'])
-        def handle_download_audio(message):
-            user = message.from_user.username
-            create_chat(message, chats)
-            chats[user].response_to_voice(message, bot)
+# maneja las entradas de texto al chat del bot
+@bot.message_handler(content_types=['text'])
+def reply_msg_handler(msg):    
+    user = msg.from_user.username
+    create_chat(msg, chats)
+
+    if msg.text.startswith('/'):
+        bot.send_message(chat_id=msg.chat.id, text='No puedo reconocer este comando 😕')
+        return
+    chats[user].response_to_text(msg, bot)
 
 
-        # maneja las pulsaciones de botones de los menús
-        @bot.callback_query_handler(func=lambda call: True)
-        def callback_query(call):
-            chat_id = call.message.chat.id
-            user = get_user_by_chat_id(chat_id, chats)
+# maneja las descargas de archivos de audio y voz 
+@bot.message_handler(content_types=['voice'])
+def handle_download_audio(message):
+    user = message.from_user.username
+    create_chat(message, chats)
+    chats[user].response_to_voice(message, bot)
 
-            if not exist(user, chats):
-                bot.send_message(chat_id=call.message.chat.id, text='No hay registro de este usuario. Presiona /newchat para iniciar una nueva conversación.')
-                return
-            bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
-            
-            files_name, contents = chats[user].get_contents_files('.json')
-            call_data = call.data.capitalize()
-            z = dict(zip(contents, files_name))
 
-            if call_data in z.keys(): # carga un contexto previamente guardado
-                chats[user].set_content_from_file(z[call_data], bot)
+# maneja las pulsaciones de botones de los menús
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    chat_id = call.message.chat.id
+    user = get_user_by_chat_id(chat_id, chats)
 
-            elif call_data in ROLES.keys(): # cambia el rol de chatgpt
-                chats[user].set_role(call_data, bot)
+    if not exist(user, chats):
+        bot.send_message(chat_id=call.message.chat.id, text='No hay registro de este usuario. Presiona /newchat para iniciar una nueva conversación.')
+        return
+    bot.delete_message(chat_id=call.message.chat.id, message_id=call.message.message_id)
+    
+    files_name, contents = chats[user].get_contents_files('.json')
+    call_data = call.data.capitalize()
+    z = dict(zip(contents, files_name))
 
-            chats[user].create_temp_file()
+    if call_data in z.keys(): # carga un contexto previamente guardado
+        chats[user].set_content_from_file(z[call_data], bot)
 
-        #--------------------------------------------------------------------------------------------------------------
-        # Activa el bot
+    elif call_data in ROLES.keys(): # cambia el rol de chatgpt
+        chats[user].set_role(call_data, bot)
 
-        bot.polling()
+    chats[user].create_temp_file()
 
-    except requests.exceptions.ReadTimeout:
-        print('timeout!')
-    except requests.exceptions.ConnectionError:
-        print('error de conexión!')
-        time.sleep(5)
+#--------------------------------------------------------------------------------------------------------------
+# Activa el bot
+
+bot.polling(non_stop=True, timeout=6000)
